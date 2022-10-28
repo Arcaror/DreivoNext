f2825b38ffbdb78723493c7fa688cd64b34139f2
Louis <louisaitidir@gmail.com>
2022-10-28 11:18:06 +0200

    protect DB informations

--- database/Database.js
+++ database/Database.js
@@ -1,6 +1,9 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('mysql://Louis:@localhost:3306/predictor')
+const sql_user = process.env.SQL_USER
+const sql_pass = process.env.SQL_PASS
+const sql_db = process.env.SQL_DB
+const sequelize = new Sequelize(`mysql://${sql_user}:${sql_pass}@localhost:3306/${sql_db}`)
 
 
 
