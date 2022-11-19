66a5dfeb7fa1717fbe987a7c068f1ff0b65a514b
Louis <louisaitidir@gmail.com>
2022-11-19 17:52:42 +0100

    Try to optimise data fetching

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import React from 'react'
 import Answer from '../components/predict/answer';
 import Head from 'next/head'
 import { useSession } from 'next-auth/react'
+import { Router } from 'next/router';
 
 
 
@@ -23,7 +24,7 @@ export default function Home(props) {
             {status == "authenticated" ? <>
                 <Answer props={props}></Answer>
 
-                </> : <>
+            </> : <>
                 <a href="/api/auth/signin">Sign in</a>
             </>}
 
@@ -31,4 +32,71 @@ export default function Home(props) {
         </div>
 
     </>)
+}
+export async function getServerSideProps(context) {
+    try {
+        await fetch('http://localhost:3000/api/auth/session')
+
+        //FIRST API REQUEST - GET SESSION
+        const res1 = await fetch('http://localhost:3000/api/session', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(context.req.cookies["next-auth.session-token"])
+
+        })
+
+        const sess = await res1.json()
+
+        //SECOND API REQUEST - GET USER
+        const res2 = await fetch('http://localhost:3000/api/user', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                userId: sess.response.userId,
+                userName: 'undefined'
+            })
+        })
+        const user = await res2.json()
+        
+        //THIRD API REQUEST - GET LAST PREDICTION
+        const res3 = await fetch('http://localhost:3000/api/predict')
+        const predi = await res3.json()
+
+        //FOUTH API REQUEST - GET LAST VOTE
+        const res4 = await fetch('http://localhost:3000/api/predict/vote', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                name: user.response.name,
+                prediId: predi.response.id
+            })
+        })
+        const vote = await res4.json()
+
+        
+        return {
+            props: {
+                sess: sess.response,
+                user: user.response,
+                predi: predi.response,
+                vote: vote.response,
+                notFound: false
+
+            }
+        }
+
+    } catch {
+        return {
+            props: {
+                notFound: true
+            }
+        }
+    }
+
 }
